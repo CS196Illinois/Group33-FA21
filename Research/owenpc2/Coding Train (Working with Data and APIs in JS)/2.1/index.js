c23a90807dd64221e5c042ae1e5c2a7739ca4222
Owen Cushing <57074635+opcushing@users.noreply.github.com>
2021-10-29 17:28:24 -0500

    research on express POST

--- Research/owenpc2/Coding Train (Working with Data and APIs in JS)/2.1/index.js
+++ Research/owenpc2/Coding Train (Working with Data and APIs in JS)/2.1/index.js
@@ -3,7 +3,32 @@
  * Tutorial from The Coding Train: https://www.youtube.com/watch?v=wxbQP1LMZsw
  * Copied by Owen Cushing (owenpc2)
  */
+const { response } = require('express');
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'))
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/api', (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      res.end();
+      return;
+    }
+    res.json(data);
+  });
+});
+
+app.post('/api', (req, res) => {
+  const data = req.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  res.json(data);
+});
